53dad3dd461351997e0deb12a8ed2fcbcf076f8e
foamerrrr <laurentchhou3@gmail.com>
2026-04-13 15:14:54 +0200

    fix

--- database/migrations/2026_04_13_130635_create_orders_table.php
+++ database/migrations/2026_04_13_130635_create_orders_table.php
@@ -16,6 +16,7 @@ return new class extends Migration
             $table->timestamps();
             $table->integer('amount');
             $table->string('status');
+            $table->foreignId('user_id')->unique()->constrained();
         });
     }
 
