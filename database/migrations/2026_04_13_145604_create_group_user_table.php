fab1aeeb54e96522330509c8cf6396d77113604c
foamerrrr <laurentchhou3@gmail.com>
2026-04-13 17:06:34 +0200

    activite3 etape 2 champ name table

--- database/migrations/2026_04_13_145604_create_group_user_table.php
+++ database/migrations/2026_04_13_145604_create_group_user_table.php
@@ -11,10 +11,9 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('groups', function (Blueprint $table) {
+        Schema::create('group_user', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
-            $table->string('name');
         });
     }
 
@@ -23,6 +22,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('groups');
+        Schema::dropIfExists('group_user');
     }
 };
