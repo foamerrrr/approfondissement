5723c5b98f37169131df3ff9a2bc121bffdfd0f7
foamerrrr <laurentchhou3@gmail.com>
2026-04-13 10:38:31 +0200

    migration settings theme lang foreignId

--- database/migrations/2026_04_13_081915_create_settings_table.php
+++ database/migrations/2026_04_13_081915_create_settings_table.php
@@ -14,6 +14,9 @@ return new class extends Migration
         Schema::create('settings', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
+            $table->theme();
+            $table->lang();
+            $table->foreignId('user_id')->unique()->constrained();
         });
     }
 
