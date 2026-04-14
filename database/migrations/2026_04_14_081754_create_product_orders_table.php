87f426e779e930a04ab4cd7177e298546123553b
foamerrrr <laurentchhou3@gmail.com>
2026-04-14 11:02:26 +0200

    activite4 step6 + fix

--- database/migrations/2026_04_14_081754_create_product_orders_table.php
+++ database/migrations/2026_04_14_081754_create_product_orders_table.php
@@ -13,6 +13,8 @@ return new class extends Migration
     {
         Schema::create('product_orders', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->decimal('price', 10, 2);
             $table->timestamps();
         });
     }
