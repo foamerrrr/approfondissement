f12fdd09ad8e915a0a4d82a420dfab6252d4d748
foamerrrr <laurentchhou3@gmail.com>
2026-04-14 10:33:34 +0200

    act4 step 1+2

---
+++ app/Models/SubscriptionOrder.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class SubscriptionOrder extends Model
+{
+    protected $fillable = [
+        'name',
+        'price',
+        'duration_months',
+    ];
+}
